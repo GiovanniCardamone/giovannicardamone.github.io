2e550845c2297bf78df3599d90beda683e4121b5
Giovanni Cardamone <g.cardamone2@gmail.com>
2020-08-01 23:41:42 +0200

    publishing

---
+++ docs/precache-manifest.3f45f3edf98954b8ef9bfd1cb93fbfd9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c01b7975d9590469344f414d454c3fb",
+    "url": "/giovannicardamone/index.html"
+  },
+  {
+    "revision": "6bc50c712eb3e1c04b65",
+    "url": "/giovannicardamone/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ddacaa1a958976405ce3",
+    "url": "/giovannicardamone/static/js/2.d3c84794.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/giovannicardamone/static/js/2.d3c84794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc50c712eb3e1c04b65",
+    "url": "/giovannicardamone/static/js/main.42f4460d.chunk.js"
+  },
+  {
+    "revision": "a2442ff70152c8489b3e",
+    "url": "/giovannicardamone/static/js/runtime-main.2e0cc7e9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/giovannicardamone/static/media/logo.5d5d9eef.svg"
+  }
+]);
